3eb41d37b0d38d34d8aaf2fcb5ed6abed6073323
Zeel <zeeldesai2163@gmail.com>
2023-05-26 13:10:48 +0530

    Selected Colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_bg: "#F6F6F6", //Seasalt
+        primary_section: "F0F6F6", //light green
+        primary_element: "#26C499", //Green
+        primary_font_light: "#FFFFFF", //White font
+        primary_font_dark: "#000000", //Black font
+      },
+    },
   },
   plugins: [],
 };
