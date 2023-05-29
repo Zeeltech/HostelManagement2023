baa2834ba8b16e7171ae82d053eaa4718eab9b8a
DhavalDudheliya <dhavaldudheliya77@gmail.com>
2023-05-29 14:26:53 +0530

    T11-Sidebar-Done

--- client/UserContext.jsx
+++ client/UserContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({ role: "Accountant" });
 
   useEffect(() => {
     if (!user) {
