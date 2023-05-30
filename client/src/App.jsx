0ad012652b0bb4a4b32d52d29d5afa8677d4a9f2
Mehul773 <114020671+Mehul773@users.noreply.github.com>
2023-05-30 12:24:55 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,9 +11,9 @@ import RectorDashboard from "./pages/Rector/RectorDashboard";
 import StudentDashboard from "./pages/Student/StudentDashboard";
 
 
-import AddFood from "./pages/Rector/Food/AddFood";
+import AddFoodPopUp from "./pages/Rector/Food/AddFoodPopUp";
 import AllFoods from "./pages/Rector/Food/AllFoods";
-import EditFood from "./pages/Rector/Food/EditFood";
+import EditFoodPopUp from "./pages/Rector/Food/EditFoodPopUp";
 import StudentProfile from "./pages/Student/StudentProfile";
 import RectorProfile from "./pages/Rector/RectorProfile";
 import AccountantProfile from "./pages/Accountant/AccountantProfile";
@@ -30,10 +30,7 @@ function App() {
           <Route path="/" element={<IndexPage />} />
           <Route path="/register" element={<UserRegister />} />
           <Route path="/login" element={<UserLogin />} />
-          <Route
-            path="/accountant/dashboard"
-            element={<AccountantDashboard />}
-          />
+       
           <Route path="/rector/dashboard" element={<RectorDashboard />} />
           <Route path="/student/dashboard" element={<StudentDashboard />} />
           <Route path="/accountant/dashboard" element={<AccountantDashboard />} />
@@ -42,9 +39,9 @@ function App() {
           <Route path="/rector/profile" element={<RectorProfile />} />
           <Route path="/accountant/profile" element={<AccountantProfile />} />
 
-          <Route path="/rector/addfood" element={<AddFood />} />
+          <Route path="/rector/addfood" element={<AddFoodPopUp />} />
           <Route path="/rector/allfoods" element={<AllFoods />} />
-          <Route path="/rector/allfoods/:id" element={<EditFood />} />
+          <Route path="/rector/allfoods/:id" element={<EditFoodPopUp />} />
 
         </Route>
       </Routes>
