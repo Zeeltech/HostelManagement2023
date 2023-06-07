dca4dafe51da8221864b198f7d277e4491c70f3e
Mehul773 <chovatiyamehul2003@gmail.com>
2023-06-07 16:03:07 +0530

    T45-Allocate-Delete-Block Done

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -43,6 +43,7 @@ const UserSchema = new mongoose.Schema(
     blockId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Block",
+      default: null,
     },
   },
   { timestamps: true, versionKey: false }
